067c5135d2ee82addcc96eb5fa1f77f55f42e0f3
Siddesh Khandekar <siddesh.204072@gmail.com>
2026-08-29 15:38:09 +0530

    chore: removed the temporary generative AST injection scripts & harden engine topology

--- fix_quote.py
+++
@@ -1,7 +0,0 @@
-import re
-with open("frontend/js/scene.js", "r", encoding="utf-8") as f:
-    code = f.read()
-code = code.replace(" ssets/porsche_911_singer_twin_turbo.glb,", " 'assets/porsche_911_singer_twin_turbo.glb',")
-with open("frontend/js/scene.js", "w", encoding="utf-8") as f:
-    f.write(code)
-print("FIXED QUOTES!")
